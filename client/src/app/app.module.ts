576fab851d5fc14f652799b3def8d098192645b7
kapepa <kapepayx@gmail.com>
2022-06-03 18:30:51 +0300

    routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from "./shared/layout/layout.module";
+import { ButtonModule } from "./shared/button/button.module";
+import { PageHomeModule } from "./page-home/page-home.module";
+import { LayoutAdminModule } from "./shared/layout-admin/layout-admin.module";
+import { LoginModule } from "./login/login.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    LayoutAdminModule,
+    ButtonModule,
+    PageHomeModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/page-home/page-home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageHomeComponent } from './page-home.component';
+
+
+
+@NgModule({
+  declarations: [
+    PageHomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PageHomeModule { }
