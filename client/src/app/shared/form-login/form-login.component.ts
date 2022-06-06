5aa3f5944870f9e5c5c0e5899d3453187c151607
kapepa <kapepayx@gmail.com>
2022-06-06 22:20:12 +0300

    login form frontend is end

--- client/src/app/shared/form-login/form-login.component.ts
+++ client/src/app/shared/form-login/form-login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {IInput} from "../../dto";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
-  styleUrls: ['./form-login.component.scss']
+  styleUrls: ['./form-login.component.scss','../input/input.component.scss']
 })
 export class FormLoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('',[
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('',[
+      Validators.minLength(6),
+    ]),
+  });
   inputs: IInput [] = [
-    { type: 'text', name: 'email', label: 'Email' },
-    { type: 'password', name: 'password', label: 'Password' },
+    { type: 'text', name: 'email', label: 'Email', data: this.email },
+    { type: 'password', name: 'password', label: 'Password', data: this.password },
   ]
 
   constructor() { }
@@ -17,7 +27,13 @@ export class FormLoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  submitForm(e: Event) {
+  onSubmit(e: Event) {
     e.preventDefault();
+    console.log('send')
   }
+
+  get email () { return this.loginForm.get('email')}
+  get password () { return this.loginForm.get('password')}
+  get validForm () { return this.loginForm.valid }
+
 }
