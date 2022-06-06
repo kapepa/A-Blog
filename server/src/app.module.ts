8570b1be71b6f1b8cdcc80fb2ebb096bf6659de9
kapepa <kapepayx@gmail.com>
2022-06-07 00:22:03 +0300

    append server and DB

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import * as dotenv from 'dotenv';
+import { User } from "./user/user.entity";
+
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+    UserModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
